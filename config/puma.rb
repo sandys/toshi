80e5dfb0964745bc8024aee621c48b60f0eb2346
sandeep srinivasa <sss+deployer@redcarpetup.com>
2015-10-10 10:49:58 +0530

    changed format of port to bind to enable unix sockets as well

--- config/puma.rb
+++ config/puma.rb
@@ -1,3 +1,5 @@
 workers Integer(ENV['PUMA_WORKERS'] || 3)
 threads Integer(ENV['PUMA_MIN_THREADS']  || 1), Integer(ENV['PUMA_MAX_THREADS'] || 16)
-port ENV['PORT'] || 5000
+bind ENV['PORT'] || 'tcp://0.0.0.0:5000'
+
+
