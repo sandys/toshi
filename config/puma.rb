28856e7be6574c353c57457c3a2bd766d5666bd0
sandeep srinivasa <sss+deployer@redcarpetup.com>
2015-11-07 14:07:57 +0530

    removing ENV['PORT'] which is used internally for sinatra. Rather use ENV['BIND'] so that we can use unix domain sockets

--- config/puma.rb
+++ config/puma.rb
@@ -1,5 +1,5 @@
 workers Integer(ENV['PUMA_WORKERS'] || 3)
 threads Integer(ENV['PUMA_MIN_THREADS']  || 1), Integer(ENV['PUMA_MAX_THREADS'] || 16)
-bind ENV['PORT'] || 'tcp://0.0.0.0:5000'
+bind ENV['BIND'] || 'tcp://0.0.0.0:5000'
 
 
